c9482474789a737e9a8b18ef715fe26324b9caa1
ferris <jake@fusetools.com>
2015-05-24 16:42:04 -0700

    playing around with args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::iter;
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    match get_file_names() {
+        Ok(file_names) => {
+            for file_name in file_names {
+                println!("{}", file_name);
+            }
+        }
+        Err(message) => {
+            println!("ERROR: {}", message);
+        }
+    }
+}
+
+fn get_file_names() -> Result<iter::Skip<env::Args>, String> {
+    let args = env::args();
+    match args.len() {
+        1 => Err("No file(s) specified".to_string()),
+        _ => Ok(args.skip(1))
+    }
 }
