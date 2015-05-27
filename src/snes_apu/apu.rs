64f6cb70fd8087e2e5e3c315d04386faf43dcba6
ferris <jake@fusetools.com>
2015-05-26 21:25:09 -0700

    love this systems language with real array types!!
    yes yes yes yes yes

--- src/snes_apu/apu.rs
+++ src/snes_apu/apu.rs
@@ -3,4 +3,16 @@ use super::timer::Timer;
 
 pub struct Apu {
     ram: [u8; 0x10000],
+    timers: [Timer; 3]
+}
+
+impl Apu {
+    pub fn new() -> Apu {
+        Apu {
+            // TODO: Randomize ram/rom contents
+            ram: [0; 0x10000],
+
+            timers: [Timer::new(256), Timer::new(256), Timer::new(256)]
+        }
+    }
 }
