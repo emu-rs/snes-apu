ae810e114c1545de2c82ba3bfa6a8a92b03fa3a9
ferris <jake@fusetools.com>
2015-05-28 18:06:58 -0700

    that should cover RingBuffer

--- src/snes_apu/ring_buffer.rs
+++ src/snes_apu/ring_buffer.rs
@@ -22,4 +22,34 @@ impl RingBuffer {
             sample_count: 0
         }
     }
+
+    pub fn set_write_pos(&mut self, pos: i32) {
+        self.write_pos = pos % (BUFFER_SIZE as i32);
+    }
+
+    pub fn set_read_pos(&mut self, pos: i32) {
+        self.read_pos = pos % (BUFFER_SIZE as i32);
+    }
+
+    pub fn write(&mut self, left: &[i16], right: &[i16], num_samples: i32) {
+        for i in 0..num_samples {
+            self.left_buffer[self.write_pos as usize] = left[i as usize];
+            self.right_buffer[self.write_pos as usize] = right[i as usize];
+            self.write_pos = (self.write_pos + 1) % (BUFFER_SIZE as i32);
+        }
+        self.sample_count += num_samples;
+    }
+
+    pub fn read(&mut self, left: &mut [i16], right: &mut [i16], num_samples: i32) {
+        for i in 0..num_samples {
+            left[i as usize] = self.left_buffer[self.read_pos as usize];
+            right[i as usize] = self.right_buffer[self.read_pos as usize];
+            self.read_pos = (self.read_pos + 1) % (BUFFER_SIZE as i32);
+        }
+        self.sample_count -= num_samples;
+    }
+
+    pub fn get_sample_count(&self) -> i32 {
+        self.sample_count
+    }
 }
