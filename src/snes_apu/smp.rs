34aad9bed84f807becf50e7dadbed8c486558bbb
ferris <jake@fusetools.com>
2015-05-28 21:26:33 -0700

    filled out lots of smp stuff

--- src/snes_apu/smp.rs
+++ src/snes_apu/smp.rs
@@ -1,3 +1,6 @@
+const RESET_VECTOR: u16 = 0xffc0;
+const RESET_REG_SP: u8 = 0xef;
+
 pub struct Smp {
     // TODO: emulator
     reg_pc: u16,
@@ -19,4 +22,74 @@ pub struct Smp {
     psw_b: bool,
 
     cycle_count: i32
+}
+
+impl Smp {
+    pub fn new() -> Smp {
+        let mut ret = Smp {
+            reg_pc: RESET_VECTOR,
+            reg_a: 0,
+            reg_x: 0,
+            reg_y: 0,
+            reg_sp: RESET_REG_SP,
+
+            psw_c: false,
+            psw_z: true,
+            psw_h: false,
+            psw_p: false,
+            psw_v: false,
+            psw_n: false,
+            psw_i: false,
+            psw_b: false,
+
+            cycle_count: 0
+        };
+        ret.reset();
+        ret
+    }
+
+    pub fn reset(&mut self) {
+        self.reg_pc = RESET_VECTOR;
+        self.reg_a = 0;
+        self.reg_x = 0;
+        self.reg_y = 0;
+        self.reg_sp = RESET_REG_SP;
+        self.set_psw(0x02);
+    }
+
+    pub fn set_reg_ya(&mut self, value: u16) {
+        self.reg_a = (value & 0xff) as u8;
+        self.reg_y = ((value >> 8) & 0xff) as u8;
+    }
+
+    pub fn get_reg_ya(&self) -> u16 {
+        ((self.reg_y as u16) << 8) | (self.reg_a as u16)
+    }
+
+    pub fn set_psw(&mut self, value: u8) {
+        self.psw_c = (value & 0x01) != 0;
+        self.psw_z = (value & 0x02) != 0;
+        self.psw_h = (value & 0x08) != 0;
+        self.psw_p = (value & 0x20) != 0;
+        self.psw_v = (value & 0x40) != 0;
+        self.psw_n = (value & 0x80) != 0;
+    }
+
+    pub fn get_psw(&self) -> u8 {
+        ((if self.psw_n { 1 } else { 0 }) << 7) |
+        ((if self.psw_v { 1 } else { 0 }) << 6) |
+        ((if self.psw_p { 1 } else { 0 }) << 5) |
+        ((if self.psw_h { 1 } else { 0 }) << 3) |
+        ((if self.psw_z { 1 } else { 0 }) << 1) |
+        (if self.psw_c { 1 } else { 0 })
+    }
+
+    fn is_negative(value: u32) -> bool {
+        (value & 0x80) != 0
+    }
+
+    fn cycles(&mut self, num_cycles: i32) {
+        // TODO: notify emulator!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        self.cycle_count += num_cycles;
+    }
 }
