b59995bedcb30c80e3a5baaa0c947bde705892ea
ferris <jake@fusetools.com>
2015-06-03 12:05:52 -0700

    fixed a logical-!-on-integer port TODO
    I believe this to be correct now. Basically the original code takes a
    value, applies a bitwise & to it with another value, then a logical
    !. In C++ the int -> bool conversion is very simple; 0 is false,
    anything else is true. The conversion back is also standard-conformant;
    false goes to 0 and true to 1. As far as I know, Rust doesn't have
    implicit int/bool conversions in either direction (which is a Good Thing
    imo), so this has to be converted to something that works without the
    logical ! operator. So basically the code is confusing but after some
    thought I think it's correct. We'll have to see when I finish the port :)

--- src/snes_apu/smp.rs
+++ src/snes_apu/smp.rs
@@ -352,11 +352,8 @@ impl<'a> Smp<'a> {
 
     fn set_bit_op(&mut self, opcode: u8) {
         let addr = self.read_pc_op();
-        // TODO: The original code uses both ~ and ! operators in these expressions.
-        // Double check the operation of these operators in Rust vs C++ and update
-        // this opcode accordingly.
         let x = self.read_dp_op(addr) & !(1 << (opcode >> 5));
-        self.write_dp_op(addr, x | (!(opcode & 0x10) << (opcode >> 5)));
+        self.write_dp_op(addr, x | ((if opcode & 0x10 == 0 { 1 } else { 0 }) << (opcode >> 5)));
     }
 
     fn test_addr_op(&mut self, x: bool) {
